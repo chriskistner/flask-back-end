57f636f8e68f3738311d4b98513a685d9bd08ccf
Christopher Kistner <chris.kistner0@gmail.com>
2019-01-28 12:08:35 -0800

    Reconfigured Models for Flask-security

--- app/app.py
+++ app/app.py
@@ -1,29 +1,50 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chris:eldar911@localhost/flask-movie-dev'
 app.debug =True
 db = SQLAlchemy(app)
 
-class User(db.Model):
+roles_users = db.Table('roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False) 
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
-    def __repr__(self):
-        return '<user %r> ' % self.username
+# Create a user to test with
+@app.before_first_request
+def create_user():
+    db.create_all()
+    user_datastore.create_user(email='chris.kistner0@gmail.com', password='password')
+    db.session.commit()
 
 @app.route('/')
 def index():
     allUsers = User.query.all()
-    for user in allUsers:
-        print(user.id, user.username, user.email)
-    return render_template('add_user.html')
+    return render_template('add_user.html', allUsers=allUsers)
+
+@app.route('/profile/<username>')
+def profile(username):
+    user = User.query.filter_by(username=username).first()
+    return render_template('profile.html', user=user)
 
 @app.route('/post_user', methods=['POST'])
 def post_user():
